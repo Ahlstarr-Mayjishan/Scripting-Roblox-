1cb2b7a189f7d102e30bf3249830179cf3df8adc
Ahlstarr-Mayjishan <ReynoldsZlotaPastevand@gmail.com>
2026-04-02 22:37:26 +0700

    feat: implement SilentAim module with metamethod hooks for combat packet redirection

--- STAR GLITCHER ~ REVITALIZED/Modules/Combat/SilentAim.lua
+++ STAR GLITCHER ~ REVITALIZED/Modules/Combat/SilentAim.lua
@@ -12,7 +12,7 @@ local SilentAim = {}
 SilentAim.__index = SilentAim
 
 local GLOBAL_HOOK_KEY = "__STAR_GLITCHER_SILENT_AIM_HOOK"
-local REDIRECT_WINDOW = 0.18
+local REDIRECT_WINDOW = 0.35
 local clock = os.clock
 
 local function isCombatRemote(remote)
@@ -26,6 +26,23 @@ local function isCombatRemote(remote)
         or mName:find("ability")
         or mName:find("target")
         or mName:find("input")
+        or mName:find("hit")
+        or mName:find("damage")
+end
+
+local function buildTargetCFrame(targetPos)
+    local camPos = Workspace.CurrentCamera.CFrame.Position
+    return CFrame.lookAt(camPos, targetPos)
+end
+
+local function buildTargetRay(origin, targetPos, length)
+    local direction = targetPos - origin
+    if direction.Magnitude <= 0.001 then
+        direction = Workspace.CurrentCamera.CFrame.LookVector
+    else
+        direction = direction.Unit * (length or (targetPos - origin).Magnitude)
+    end
+    return Ray.new(origin, direction)
 end
 
 local function ensureHookState()
@@ -46,16 +63,15 @@ local function ensureHookState()
         if selfRef
             and not selfRef._destroyed
             and not checkcaller()
-            and selfRef:_isRedirectActive() then
+            and selfRef:_hasTargetLock() then
             if inst == Mouse or (typeof(inst) == "Instance" and inst:IsA("Mouse")) then
                 if index == "Hit" then
-                    local camPos = Workspace.CurrentCamera.CFrame.Position
-                    return CFrame.lookAt(camPos, selfRef.TargetPosCache)
+                    return buildTargetCFrame(selfRef.TargetPosCache)
                 elseif index == "Target" then
                     return selfRef.TargetPartCache
                 elseif index == "UnitRay" then
                     local camPos = Workspace.CurrentCamera.CFrame.Position
-                    return Ray.new(camPos, (selfRef.TargetPosCache - camPos).Unit)
+                    return buildTargetRay(camPos, selfRef.TargetPosCache, 1)
                 end
             end
         end
@@ -72,38 +88,49 @@ local function ensureHookState()
         if selfRef
             and not selfRef._destroyed
             and not checkcaller()
-            and selfRef:_isRedirectActive()
-            and (method == "FireServer" or method == "InvokeServer")
-            and isCombatRemote(inst) then
-            local modified = false
-
-            for i = 1, args.n do
-                local arg = args[i]
-                if typeof(arg) == "Vector3" then
-                    args[i] = selfRef.TargetPosCache
-                    modified = true
-                elseif typeof(arg) == "Instance" and (arg:IsA("BasePart") or arg:IsA("Model")) then
-                    local localCharacter = LocalPlayer.Character
-                    if not (localCharacter and arg:IsDescendantOf(localCharacter)) then
-                        args[i] = selfRef.TargetPartCache
-                        modified = true
-                    end
-                elseif typeof(arg) == "CFrame" then
-                    local origin = arg.Position
-                    local direction = selfRef.TargetPosCache - origin
-                    if direction.Magnitude > 0.001 then
-                        args[i] = CFrame.lookAt(origin, selfRef.TargetPosCache)
-                        modified = true
-                    end
-                elseif typeof(arg) == "Ray" then
-                    args[i] = Ray.new(arg.Origin, (selfRef.TargetPosCache - arg.Origin).Unit * arg.Direction.Magnitude)
-                    modified = true
-                end
+            and selfRef:_hasTargetLock() then
+            if (method == "ViewportPointToRay" or method == "ScreenPointToRay") and inst == Workspace.CurrentCamera then
+                local camPos = Workspace.CurrentCamera.CFrame.Position
+                return buildTargetRay(camPos, selfRef.TargetPosCache, 1)
             end
 
-            if modified then
-                selfRef._lastRedirectTime = clock()
-                return oldNamecall(inst, unpack(args, 1, args.n))
+            if selfRef:_isRedirectActive() then
+                if (method == "FindPartOnRay" or method == "FindPartOnRayWithIgnoreList" or method == "FindPartOnRayWithWhitelist") and inst == Workspace then
+                    local ray = args[1]
+                    if typeof(ray) == "Ray" then
+                        args[1] = buildTargetRay(ray.Origin, selfRef.TargetPosCache, ray.Direction.Magnitude)
+                        return oldNamecall(inst, unpack(args, 1, args.n))
+                    end
+                end
+
+                if (method == "FireServer" or method == "InvokeServer") and isCombatRemote(inst) then
+                    local modified = false
+
+                    for i = 1, args.n do
+                        local arg = args[i]
+                        if typeof(arg) == "Vector3" then
+                            args[i] = selfRef.TargetPosCache
+                            modified = true
+                        elseif typeof(arg) == "Instance" and (arg:IsA("BasePart") or arg:IsA("Model")) then
+                            local localCharacter = LocalPlayer.Character
+                            if not (localCharacter and arg:IsDescendantOf(localCharacter)) then
+                                args[i] = selfRef.TargetPartCache
+                                modified = true
+                            end
+                        elseif typeof(arg) == "CFrame" then
+                            args[i] = buildTargetCFrame(selfRef.TargetPosCache)
+                            modified = true
+                        elseif typeof(arg) == "Ray" then
+                            args[i] = buildTargetRay(arg.Origin, selfRef.TargetPosCache, arg.Direction.Magnitude)
+                            modified = true
+                        end
+                    end
+
+                    if modified then
+                        selfRef._lastRedirectTime = clock()
+                        return oldNamecall(inst, unpack(args, 1, args.n))
+                    end
+                end
             end
         end
 
@@ -131,8 +158,12 @@ function SilentAim.new(config, synapse)
     return self
 end
 
+function SilentAim:_hasTargetLock()
+    return self.Active and self.TargetPosCache ~= nil and self.TargetPartCache ~= nil
+end
+
 function SilentAim:_isRedirectActive()
-    if not (self.Active and self.TargetPosCache and self.TargetPartCache) then
+    if not self:_hasTargetLock() then
         return false
     end
 
